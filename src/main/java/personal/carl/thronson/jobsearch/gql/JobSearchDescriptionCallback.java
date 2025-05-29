71d08f38260a1ded628b61a175897d28634ea2b5
Carl Thronson <carlthronson@gmail.com>
2025-05-28 19:10:15 -0700

    Try to cleanup open connections

--- src/main/java/personal/carl/thronson/jobsearch/gql/JobSearchDescriptionCallback.java
+++ src/main/java/personal/carl/thronson/jobsearch/gql/JobSearchDescriptionCallback.java
@@ -1,6 +1,8 @@
 package personal.carl.thronson.jobsearch.gql;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.jsoup.Jsoup;
@@ -11,6 +13,7 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
+import personal.carl.thronson.http.JobSummaryReader;
 import personal.carl.thronson.jobsearch.data.entity.JobSearchJobDescriptionEntity;
 import personal.carl.thronson.jobsearch.data.entity.JobSearchJobListingEntity;
 import reactor.core.publisher.MonoSink;
@@ -51,11 +54,10 @@ public class JobSearchDescriptionCallback implements Callback {
     sink.error(e);
   }
 
-  @Override
-  public void onResponse(Call call, Response response) throws IOException {
+//  @Override
+  public void _onResponse(Call call, Response response) throws IOException {
     if (response.isSuccessful()) {
-      try {
-        ResponseBody responseBody = response.body();
+      try (ResponseBody responseBody = response.body()) {
         String body = responseBody.string();
         Document doc = Jsoup.parse(body);
         Elements jobCriteriaItems = doc.getElementsByClass("description__job-criteria-item");
@@ -80,11 +82,50 @@ public class JobSearchDescriptionCallback implements Callback {
         sink.success(jobMetaData);
       } catch (Exception ex) {
         ex.printStackTrace();
-        sink.error(new IOException("Error parsing response body", ex));
+//        sink.error(new IOException("Error parsing response body", ex));
+        sink.success(jobMetaData);
       }
     } else {
       System.out.println("Response was unsuccessful: " + response.code());
-      sink.error(new IOException("Unsuccessful response: " + response));
+//      sink.error(new IOException("Unsuccessful response: " + response));
+      sink.success(jobMetaData);
+    }
+  }
+
+  @Override
+  public void onResponse(Call call, Response response) throws IOException {
+//    logger.info("Job search list callback onResponse: " + response);
+    try (ResponseBody responseBody = response.body()) {
+      if (!response.isSuccessful()) {
+        sink.error(new IOException("Unexpected code " + response));
+      } else {
+        if (responseBody != null) {
+          String body = responseBody.string();
+          Document doc = Jsoup.parse(body);
+          Elements jobCriteriaItems = doc.getElementsByClass("description__job-criteria-item");
+          jobCriteriaItems.forEach(jobCriteriaItem -> {
+            Elements subheaders = jobCriteriaItem.getElementsByClass("description__job-criteria-subheader");
+            subheaders.forEach(subheader -> {
+              if (subheader.text().contains("Employment type")) {
+                Elements subText = jobCriteriaItem.getElementsByClass("description__job-criteria-text");
+                String employmentType = subText.text();
+                jobMetaData.setEmploymentType(employmentType);
+              }
+            });
+          });
+          Elements hiddenElements = doc.getElementsByClass("show-more-less-html__markup");
+          String description = hiddenElements.text();
+          if (description != null && description.trim().length() > 0) {
+            JobSearchJobDescriptionEntity descriptionEntity = new JobSearchJobDescriptionEntity();
+            descriptionEntity.setListing(jobMetaData);
+            descriptionEntity.setDescription(description);
+            jobMetaData.setDescription(descriptionEntity);
+          }
+          sink.success(jobMetaData);
+        } else {
+          sink.success(jobMetaData);
+        }
+      }
     }
   }
 }
